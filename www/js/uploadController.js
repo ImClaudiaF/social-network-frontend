9b679e4b9f5021d86727ff4e94ca4fddca1047b1
ImClaudiaF <fclaudia.93@gmail.com>
2016-07-10 00:48:46 -0400

    Tenth commit. Geolocation

--- www/js/uploadController.js
+++ www/js/uploadController.js
@@ -1,17 +1,50 @@
 app.controller('uploadController', function($http, $scope){
 	var geo;
-	$scope.geolocation = function() {
-		navigator.geolocation.getCurrentPosition(function(position){
-			geo = position.coords.latitude +', ' + position.coords.longitude;
-			return geo;
-		});
+	// $scope.geolocation = function() {
+	// 	var options = {
+	// 		enableHighAccuracy: true,
+ //     	 	maximumAge: 3600000
+ //   		}
+	
+	// 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+ //   		function onSuccess(position) {
+	//    		geo = position.coords.latitude +', ' + position.coords.longitude;
+	//    	};
+
+	//    	function onError(error) {
+	//       	alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+	//    	}
+	// },
+	$scope.geo = {
+		getGeo: function(){
+			if($scope.enableGeo){
+				var options = {
+					enableHighAccuracy: true,
+     	 			maximumAge: 3600000
+   				}
+				var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+		   		function onSuccess(position) {
+			   		geo = position.coords.latitude +', ' + position.coords.longitude;
+			   		//alert(geo);
+			   	};
+
+			   	function onError(error) {
+			      	alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+			   	}
+			}					
+		}
 	},
 	$scope.upload = {
 		uploadFile: function(){
 			var x = document.getElementById('file');
 			var fd = new FormData();
+
 			if($scope.tag==undefined)
 				$scope.tag = "";
+			if(geo==undefined)
+				geo = "";
 			fd.append('username', localStorage.getItem('username'));
 			fd.append('title', $scope.title);	
 			fd.append('tag', $scope.tag);
