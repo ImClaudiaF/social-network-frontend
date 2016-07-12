52c8fe1041db456279b5860a270698c3a48f75ad
ImClaudiaF <fclaudia.93@gmail.com>
2016-07-11 20:43:05 -0400

    12th

--- www/js/uploadController.js
+++ www/js/uploadController.js
@@ -1,111 +1,111 @@
-app.controller('uploadController', function($http, $scope){
-	var geo;
-	// $scope.geolocation = function() {
-	// 	var options = {
-	// 		enableHighAccuracy: true,
- //     	 	maximumAge: 3600000
- //   		}
-	
-	// 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+var blob;
+app.controller('uploadController', function($http, $scope) {
+    var geo;
+    $scope.getGeo = function() {
+        if ($scope.enableGeo) {
+            var options = {
+                enableHighAccuracy: true,
+                maximumAge: 3600000
+            }
+            var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
- //   		function onSuccess(position) {
-	//    		geo = position.coords.latitude +', ' + position.coords.longitude;
-	//    	};
+            function onSuccess(position) {
+                geo = position.coords.latitude + ', ' + position.coords.longitude;
+                //alert(geo);
+            };
 
-	//    	function onError(error) {
-	//       	alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-	//    	}
-	// },
-	$scope.geo = {
-		getGeo: function(){
-			if($scope.enableGeo){
-				var options = {
-					enableHighAccuracy: true,
-     	 			maximumAge: 3600000
-   				}
-				var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+            function onError(error) {
+                alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+            }
+        }
+    }
+    $scope.uploadFile = function() {
+        var x = document.getElementById('file');
+        var fd = new FormData();
 
-		   		function onSuccess(position) {
-			   		geo = position.coords.latitude +', ' + position.coords.longitude;
-			   		//alert(geo);
-			   	};
+        if ($scope.tag == undefined)
+            $scope.tag = "";
+        if (geo == undefined)
+            geo = "";
+        fd.append('username', localStorage.getItem('username'));
+        fd.append('title', $scope.title);
+        fd.append('tag', $scope.tag);
+        fd.append('geolocation', geo);
+        fd.append('file', x.files[0]);
+        $http({
+                url: 'http://192.168.1.137:10000/api/passfile',
+                method: 'POST',
+                headers: {
+                    "content-type": undefined
+                },
+                data: fd
+            })
+            .success(function(data) {
+                console.log(data);
+            });
+    }
+    $scope.takePicture = function() {
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
 
-			   	function onError(error) {
-			      	alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-			   	}
-			}					
-		}
-	};
-	$scope.upload = {
-		uploadFile: function(){
-			var x = document.getElementById('file');
-			var fd = new FormData();
-
-			if($scope.tag==undefined)
-				$scope.tag = "";
-			if(geo==undefined)
-				geo = "";
-			fd.append('username', localStorage.getItem('username'));
-			fd.append('title', $scope.title);	
-			fd.append('tag', $scope.tag);
-			fd.append('geolocation', geo);
-			fd.append('file', x.files[0]);
-			//fd.append('type', x.files[0].type);
-			$http({
-				url: 'http://192.168.1.137:10000/api/passfile',
-				method: 'POST',
-				headers: {
-					"content-type": undefined
-				},
-				data: fd
-			})
-			.success(function(data){
-				console.log(data);
-				//transicion
-			});
-		},
-		takePicture: function(){
-			console.log('antes');
-			navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-			    destinationType: Camera.DestinationType.DATA_URL
-			});
-			 console.log('despues');
-
-			function onSuccess(imageData) {
-			    var image = document.getElementById('myImage');
-			    image.src = "data:image/jpeg;base64," + imageData;
-			}
-
-			function onFail(message) {
-			    alert('Failed because: ' + message);
-			}
+        $scope.myImg = {
+            src: ""
+        };
 
 
-			// console.log('antes');
-			// navigator.camera.getPicture(onSuccess, onFail, {
-	  //                quality: 50,
-	  //                destinationType: Camera.DestinationType.DATA_URL
-	  //           });
+        function onSuccess(imageData) {
+            $scope.myImg.src = "data:image/jpeg;base64," + imageData;
+            $scope.pp = imageData;
 
-			// console.log('despues');
+            var imageBase64 = imageData;
+            blob = new Blob([imageBase64], {
+                type: 'image/jpeg'
+            });
+            console.log(blob);
 
-			// $scope.myImg = {
-   //          	src: ""
-   //      	};
+            var fd = new FormData();
 
-	  //       function onSuccess(imageData) {
-	  //            $scope.myImg.src = "data:image/jpeg;base64," + imageData;     
-	  //            console.log($scope.myImg.src);        
-	  //            $scope.$apply();
-	  //       }
+            if ($scope.tag == undefined)
+                $scope.tag = "";
+            if (geo == undefined)
+                geo = "";
+            fd.append('username', localStorage.getItem('username'));
+            fd.append('title', $scope.title);
+            fd.append('tag', $scope.tag);
+            fd.append('geolocation', geo);
+            fd.append('file', blob);
+            $http({
+                    url: 'http://192.168.1.137:10000/api/passfile',
+                    method: 'POST',
+                    headers: {
+                        "content-type": undefined
+                    },
+                    data: fd
+                })
+                .success(function(data) {
+                    console.log(data);
+                });
 
-	  //       function onFail(message) {
-	  //           alert('Failed because: ' + message);
-	  //       }
+            // var contentType = 'image/jpeg';
+            // var b64Data = imageData;
 
-	            
-	        
+            // var blob = b64toBlob(b64Data, contentType);
+            // var blobUrl = URL.createObjectURL(blob);
+            // console.log(blob);
+            // console.log(blobUrl);
 
-		}
-	}
+            // var img = document.createElement('img');
+            // img.src = blobUrl;
+            // document.body.appendChild(img);
+
+
+            $scope.$apply();
+        };
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    }
 })
