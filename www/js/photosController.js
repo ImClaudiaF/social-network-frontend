9dfd85b6fce40241fd38e4197bb4b15733159487
ImClaudiaF <fclaudia.93@gmail.com>
2016-07-16 19:37:06 -0400

    16th Commit

--- www/js/photosController.js
+++ www/js/photosController.js
@@ -7,7 +7,7 @@ app.controller('photosController', function($http, $scope, $state, $sce) {
             flag = true;
             console.log('pg '+pg);
             console.log(flag);
-            $http.get("http://192.168.1.137:10000/info_publish/0")//)/" + localStorage.getItem('token') + "/" + localStorage.getItem('username'))
+            $http.get("http://192.168.1.137:10000/info_publish/0/" + localStorage.getItem('token') + "/" + localStorage.getItem('username'))
                 .success(function(data) {
                     console.log(data);
                     tpg = data.response[0].count;
@@ -26,7 +26,7 @@ app.controller('photosController', function($http, $scope, $state, $sce) {
                         if (pg < tpg) {
                             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
                                 console.log(pg);
-                                $http.get("http://192.168.1.137:10000/info_publish/" + (pg * 5))// + "/" + localStorage.getItem('token') + "/" + localStorage.getItem('username'))
+                                $http.get("http://192.168.1.137:10000/info_publish/" + (pg * 5) + "/" + localStorage.getItem('token') + "/" + localStorage.getItem('username'))
                                     .success(function(data) {
                                         console.log(data);
                                         $scope.uploads = data.response.map(function(el) {
